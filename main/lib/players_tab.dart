ceb41d55cb95c625a9b24aab6b2ce5518914b68b
Robobie Estan <robbie.e735@gmail.com>
2024-10-04 18:09:07 -0400

    I changed the players tab so that it requires a list of players as input but didn't change the main tab to not mess with other peoples work

--- main/lib/players_tab.dart
+++ main/lib/players_tab.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'player.dart';
 
 class PlayersTab extends StatefulWidget{
-  const PlayersTab({super.key});
+  const PlayersTab({super.key, required this.players});
+
+  final List<Player> players; 
 
   @override
   State<PlayersTab> createState() => _PlayersTab();
@@ -11,6 +14,38 @@ class _PlayersTab extends State<PlayersTab> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Not Implemented',),);
+    return SafeArea(child: ListView.builder(
+      itemCount: 8,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primaryFixedDim,),
+                child: ListTile(
+                  title: Text(widget.players[index].name),
+                  leading: Container(
+                    width: 70,
+                    height: 70,
+                    child: Image(
+                        image: NetworkImage(
+                      widget.players[index].image,
+                    )),
+                  ),
+                  subtitle: Text(widget.players[index].name),
+                  //onTap: () {
+                  //  Navigator.push(
+                  //      context,
+                  //      MaterialPageRoute(
+                  //          builder: (context) =>
+                  //              DisplayTeam(team: widget.teams[index])));
+                  //},
+                ),
+              ),
+            );
+          }
+           
+      )
+    );
   }
 }
