1cab78b1be51a5ac76a2cf4b4caed6bacbe6b647
Sam Kim <skim2898@umd.edu>
2024-10-04 16:10:24 -0400

    Scores Tab checkpoint 1

--- main/lib/scores_tab.dart
+++ main/lib/scores_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:main/game_details.dart';
 
 class ScoresTab extends StatefulWidget{
   const ScoresTab({super.key});
@@ -11,6 +12,42 @@ class _ScoresTab extends State<ScoresTab> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Not Implemented',),);
+    final items = ['Ravens vs Steelers (24 - 17)', 'Buccaneers vs Falcons (31-21)', 'Lions vs Seahawks (14-17)'];
+    final icons = [Icons.sports_football_outlined, Icons.sports_football_outlined, Icons.sports_football_outlined];
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: LayoutBuilder(
+            builder: (context, constraints) => GestureDetector(
+              child:
+                Text(items[index]),
+              onTap: () {
+                Navigator.push(
+                  context,
+                    MaterialPageRoute(
+                      builder: (context) => const OtherView(),
+                    )
+                );
+              },
+            )
+          ),
+          leading: LayoutBuilder(
+            builder: (context, constraints) => GestureDetector(
+              child:
+                Icon(icons[index]),
+              onTap: () {
+                Navigator.push(
+                  context,
+                    MaterialPageRoute(
+                      builder: (context) => const OtherView(),
+                    )
+                );
+              },
+            )
+          ),     
+        );
+      },
+    );
   }
 }
