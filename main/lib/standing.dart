081a8095414c89859511ce041b7c38adfbaf27ed
Robert Estan <robbie.e735@gmail.com>
2025-01-04 13:15:52 -0800

    Update standing.dart

--- main/lib/standing.dart
+++ main/lib/standing.dart
@@ -1,1 +1,27 @@
+class Standing {
+  final String name;
+  final String logo;
+  final int position;
+  final int wins;
+  final int losses;
+  final int ties;
+  final int pointsFor;
+  final int pointsAgainst;
+  final int netPoints;
+  final String streak;
+  final int id;
 
+  Standing({
+    required this.name,
+    required this.logo,
+    required this.position,
+    required this.wins,
+    required this.losses,
+    required this.ties,
+    required this.pointsFor,
+    required this.pointsAgainst,
+    required this.netPoints,
+    required this.streak,
+    required this.id,
+  });
+}
