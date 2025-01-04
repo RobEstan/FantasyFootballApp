2300e50d340a1794e2b08d1fac447175f0f27b52
Robert Estan <robbie.e735@gmail.com>
2025-01-04 14:40:25 -0800

    Update drawer_model.dart

--- main/lib/drawer_model.dart
+++ main/lib/drawer_model.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-class DrawerModel extends ChangeNotifier{
-  bool notificationsOn = true;
-
-  void toggleNotifications(bool value) async{
-    notificationsOn = value;
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool('notifsOn', value);
-    notifyListeners();
-  }
-
-  void setNotificationsOn() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    notificationsOn = prefs.getBool('notifsOn') ?? true;
-  }
-}
